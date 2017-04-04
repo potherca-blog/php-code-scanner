c9b6225b60caaaaff9f0348a8fbfcd856592765d
Ben Peachey <potherca@gmail.com>
2017-04-04 22:28:16 +0000

    First pass at finding function calls.

--- src/Scanner.php
+++ src/Scanner.php
@@ -8,6 +8,8 @@ class Scanner
 {
     /** @var Filesystem */
     private $filesystem;
+    /** @var array */
+    private $result;
 
     final public function __construct(Filesystem $filesystem)
     {
@@ -17,28 +19,77 @@ class Scanner
     final public function scan($folder)
     {
         $contents = $this->filesystem->listContents($folder, true);
-        
+
         foreach ($contents as $item) {
-            // @TODO: if ($item['type'] === 'folder') {}
-            $this->parse($item);
+            $path = $item['path'];
+            // if ($item['type'] === 'folder') {
+            // @TODO: ...recurse...
+            //} else {
+
+            $contents = $this->filesystem->read($path);
+
+            $this->result[$path] = $this->parse($contents);
+
+            //)
         }
+
+        return $this->result;
     }
 
-    private function parse($item)
+    private function parse($contents)
     {
-        // get file content
-        $contents = $this->filesystem->read($item['path']);
+        $result = [
+            'declared' => [],
+            'call-internal' => [],
+            'call-userland' => [],
+        ];
+
         $tokens = token_get_all($contents);
-        
-        foreach ($tokens as &$token) {
-            $token[0] = token_name($token[0]);
+
+        foreach ($tokens as $index => $token) {
+            if (is_string($token[0]) === false) {
+                $token[0] = token_name($token[0]);
+                $tokens[$index][0] = $token[0];
+            }
+
+            switch ($token[0]) {
+                /*/ function definition /*/
+                case 'T_FUNCTION':
+                    $functionName = $tokens[($index + 2)];
+                    $result['declared'][] = $functionName[1];
+                break;
+
+                /*/ potential function usage /*/
+                case 'T_STRING':
+                    $candidate = $token[1];
+
+                    if ($this->isInternalFunction($candidate)) {
+                        $result['call-internal'][] = $candidate;
+                    } else {
+                        // @FIXME: Some of these are class methods!
+                        $result['call-userland'][] = $candidate;
+                    }
+                break;
+            }
         }
-        
-        // T_STRING = function usage
-        // T_FUNCTION = "function" keyword
-        var_dump($tokens);exit;
-        // create AST from content
-        // filter relevant fields from AST
+
+        $result['declared'] = array_unique($result['declared']);
+        $result['call-internal'] = array_unique($result['call-internal']);
+        $result['call-userland'] = array_unique($result['call-userland']);
+
+        return $result;
+    }
+
+    private function isInternalFunction($functionName)
+    {
+        static $internalFunctions;
+
+        if ($internalFunctions === null) {
+            $allFunctions = get_defined_functions();
+            $internalFunctions = $allFunctions['internal'];
+        }
+
+        return in_array($functionName, $internalFunctions);
     }
 }
 
