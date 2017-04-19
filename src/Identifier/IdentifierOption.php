436f34283f5be29925d29f0e192d8e74a148cc4e
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-04-19 16:55:01 +0200

    Adds enum class listing which options are available for Identifiers.

---
+++ src/Identifier/IdentifierOption.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Potherca\Scanner\Identifier;
+
+use Potherca\Scanner\AbstractEnum;
+
+class IdentifierOption extends AbstractEnum
+{
+    const IDENTIFIERS = 'identifier';
+    const INTERNAL_IDENTIFIERS = 'internal-identifiers';
+    const PHP_VERSION = 'php-version';
+}
+
+/*EOF*/
