499614a1604cd11ddd544584ad2b51b465c5e2fa
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-04-19 16:53:30 +0200

    Changes ArgumentInterface to include identifiers and PHP version.

--- src/ArgumentInterface.php
+++ src/ArgumentInterface.php
@@ -12,6 +12,12 @@ interface ArgumentInterface
 
     /** @return array */
     public function getWhitelist();
+
+    /** @return int */
+    public function getPhpVersion();
+
+    /** @return array */
+    public function getIdentifiers();
 }
 
 /*EOF*/
