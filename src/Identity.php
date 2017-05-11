715c00f10159c1da3ba042cb5d5dfc59285ee1fc
Ben Peachey <ben.peachey@dealerdirect.nl>
2017-05-11 15:41:03 +0200

    Adds the ability to store an identities location to the Identity class.

--- src/Identity.php
+++ src/Identity.php
@@ -2,25 +2,75 @@
 
 namespace Potherca\Scanner;
 
+use PhpParser\Node;
+use Potherca\Scanner\Exception\InvalidArgumentException;
+
 class Identity
 {
     ////////////////////////////// CLASS PROPERTIES \\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    const ERROR_PROPERTY_NOT_SET = 'Can not get property that has not been set: "%s"';
+    const ERROR_REQUIRED_NOT_SET = 'Not al required properties have been set.';
+    const ERROR_UNSUPPORTED_KEYS = 'Unsupported keys "%s". Supported keys are: "%s"';
+
     /** @var array */
-    private $identities;
+    private /** @noinspection PropertyCanBeStaticInspection */ $availableProperties = [
+        'class' => '',
+        'file' => 'REQUIRED',
+        'function' => '',
+        'identity' => 'REQUIRED',
+        'namespace' => 'REQUIRED',
+        'value' => 'REQUIRED',
+    ];
+    /** @var Node */
+    private $node;
+    /** @var array */
+    private $properties;
 
     //////////////////////////// SETTERS AND GETTERS \\\\\\\\\\\\\\\\\\\\\\\\\\\
+
     /**
      * @return array
      */
-    final public function getIdentities()
+    final public function getIdentity()
     {
-        return $this->identities;
+        /** @noinspection ExceptionsAnnotatingAndHandlingInspection */// Key is guarantied to be present.
+        return $this->getProperty('identity');
+    }
+
+    /**
+     * @return array
+     */
+    private function getProperties()
+    {
+        return $this->properties;
     }
 
     //////////////////////////////// PUBLIC API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-    final public function __construct(array $identities, $value)
+    final public function __construct(Node $node, array $properties)
     {
-        $this->identities = $identities;
+        $this->validate($properties);
+
+        $this->node = $node;
+        $this->properties = $properties;
+    }
+
+    final public function __toString()
+    {
+        try {
+            $output = vsprintf('[%s] %s:%s %s%s%s = "%s" (%s)', [
+                implode(',', $this->getProperty('identity')),
+                $this->getProperty('file'),
+                $this->getProperty('line'),
+                $this->getProperty('namespace'),
+                $this->getProperty('class'),
+                $this->getProperty('function'),
+                $this->getProperty('value'),
+                $this->getProperty('node-type'),
+            ]);
+        } catch (\Exception $exception) {
+            $output = 'ERROR: '.$exception->getMessage();
+        }
+        return (string) $output;
     }
 
     /**
@@ -30,10 +80,108 @@ class Identity
      */
     final public function hasIdentity($identity)
     {
-        return in_array($identity, $this->identities, true);
+        return in_array($identity, $this->getIdentity(), true);
     }
 
     ////////////////////////////// UTILITY METHODS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    /**
+     * @param string $key
+     *
+     * @return mixed
+     *
+     * @throws \Potherca\Scanner\Exception\InvalidArgumentException
+     */
+    private function getProperty($key)
+    {
+        $property = null;
+
+        switch ($key) {
+
+            case 'class':
+                $property = $this->properties['class']
+                    ? $this->properties['class'] . '::'
+                    : '';
+                break;
+
+            case 'function':
+                $property = $this->properties['function']
+                    ?: ''
+                ;
+                break;
+
+            case 'line':
+                $start = $this->node->getAttribute('startLine');
+                $end = $this->node->getAttribute('endLine');
+
+                if ($start === $end) {
+                    $property = $start;
+                } else {
+                    $property = $start .'-'. $end;
+                }
+                break;
+
+            case 'namespace':
+                $property = $this->properties['namespace'] . '\\';
+                break;
+
+            case 'node-type':
+                $property = $this->node->getType();
+                break;
+
+            default:
+                $properties = $this->getProperties();
+
+                if (array_key_exists($key, $properties) === false) {
+                    $message = sprintf(self::ERROR_PROPERTY_NOT_SET, $key);
+
+                    throw new InvalidArgumentException($message);
+                } else {
+                    $property = $properties[$key];
+
+                }
+                break;
+        }
+
+        return $property;
+    }
+
+    /**
+     * @param array $properties
+     *
+     * @throws \Potherca\Scanner\Exception\InvalidArgumentException
+     */
+    private function validate(array $properties)
+    {
+        $message = null;
+
+        $availableProperties = $this->availableProperties;
+        $propertyDiff = array_diff_key($properties, $availableProperties);
+
+        if (count($propertyDiff) !== 0) {
+            $message = vsprintf(self::ERROR_UNSUPPORTED_KEYS, [
+                implode('", "', array_keys($propertyDiff)),
+                implode('", "', array_keys($availableProperties)),
+            ]);
+        } else {
+            $required = array_filter($this->availableProperties, function ($value) {
+                return $value === 'REQUIRED';
+            });
+
+            $requiredDiff = array_diff_key($required, $properties);
+
+            if (count($requiredDiff) !== 0) {
+                $message = vsprintf('%s Please provide: "%s"', [
+                    self::ERROR_REQUIRED_NOT_SET,
+                    implode('", "', array_keys($requiredDiff)),
+                ]);
+            }
+        }
+
+        if ($message !== null) {
+            throw new InvalidArgumentException($message);
+        }
+    }
 }
 
 /*EOF*/
